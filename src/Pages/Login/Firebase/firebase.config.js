c887b2cf972d38545995d14a82af23e7825d8beb
tithi23 <tithiratry@gmail.com>
2021-11-16 02:10:38 -0800

    added backend database server

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyAC5_MT9WI63mJbYEd1xwHLVdJhr4j51mA",
+    authDomain: "genius-car-mechanics-62ade.firebaseapp.com",
+    projectId: "genius-car-mechanics-62ade",
+    storageBucket: "genius-car-mechanics-62ade.appspot.com",
+    messagingSenderId: "635641428873",
+    appId: "1:635641428873:web:a443c1ef6d5b33435cbf58"
+};
+
+export default firebaseConfig;
